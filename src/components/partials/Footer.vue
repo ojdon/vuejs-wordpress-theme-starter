e5985ee97b3a7efcb630b9d1e3e71467a8b21caf
Ollie Reardon <oliver.reardon@rapidweb.biz>
2018-02-07 16:19:32 +0000

    Header and Footer templates w/ Menu integrated

--- src/components/partials/Footer.vue
+++ src/components/partials/Footer.vue
@@ -1,5 +1,30 @@
 <template>
-    <footer class="footer">
-        This is the footer.
+    <footer class="footer text-center">
+      <wpmenu name="header_menu" class="my-5 "></wpmenu>
+       <p class="mb-5">&copy; {{ new Date().getFullYear() }} - {{name}}</p>
     </footer>
 </template>
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      name: '',
+      errors: []
+    }
+  },
+
+  // Fetches posts when the component is created.
+  created() {
+    window.axios.get('/wp-json/')
+    .then(response => {
+      // JSON responses are automatically parsed.
+      this.name = response.data.name
+    })
+    .catch(e => {
+      this.errors.push(e)
+    })
+  }
+}
+</script>
